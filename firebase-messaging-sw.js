3f3090684e029a282ee923c87f596d3654f7fd8d
RobertoMatCab <36885528+RobertoMatCab@users.noreply.github.com>
2020-03-18 18:19:50 +0100

    001

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,32 +1,14 @@
-importScripts("/servidor/precache-manifest.da10eeb062b35db3eb10abb03b28921a.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
-importScripts('https://www.gstatic.com/firebasejs/5.6.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/5.6.0/firebase-messaging.js');
-
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-//workbox.precaching.suppressWarnings();
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-
-workbox.routing.registerRoute(
-    new RegExp('https://firebasestorage.googleapis.com/v0/b/vue-firebase-tutorial-7ee46.appspot.com/.*'),
-    workbox.strategies.staleWhileRevalidate()
-);
-
+// [START initialize_firebase_in_sw]
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-messaging.js');
+// Initialize the Firebase app in the service worker by passing in the messagingSenderId.
 firebase.initializeApp({
-    'messagingSenderId': '24087531277'
+    'messagingSenderId': '186310212762' // 4. Get Firebase Configuration
 });
 
+// Retrieve an instance of Firebase Messaging so that it can handle background messages.
 const messaging = firebase.messaging();
-
-messaging.setBackgroundMessageHandler(function(payload) {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    // Customize notification here
-    const notificationTitle = 'Background Message Title';
-    const notificationOptions = {
-        body: 'Background Message body.',
-        icon: '/firebase-logo.png'
-    };
-
-    return self.registration.showNotification(notificationTitle,
-        notificationOptions);
-});
+// [END initialize_firebase_in_sw]
---
+++ precache-manifest.260ce761b097bcba86708388aa4f2c89.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9c20840abad6286bb0b",
+    "url": "/css/app.703e8d87.css"
+  },
+  {
+    "revision": "89c2a1cfc5b9893c4bbc7f3efd9ec15e",
+    "url": "/firebase-messaging-sw.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "373f32adba67a27da907493fea5251d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e8143d34ed75f5507b4",
+    "url": "/js/about.44be5cdf.js"
+  },
+  {
+    "revision": "d9c20840abad6286bb0b",
+    "url": "/js/app.2e71da99.js"
+  },
+  {
+    "revision": "6b74d591e71ec32867df",
+    "url": "/js/chunk-vendors.68068867.js"
+  },
+  {
+    "revision": "60719a3f646e67f5d46dbbfef814656e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
